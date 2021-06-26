a5f2d74202ed615920ca172a597e5a431b4d8721
夏2同学 <1754082565@qq.com>
2021-06-26 14:39:42 +0800

    init

--- upload.sh
+++ upload.sh
@@ -1,7 +1,5 @@
-echo "# vue-search-any-where" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:xia-2/vue-search-any-where.git
+git add .
+
+git commit -m "init"
+
 git push -u origin main
